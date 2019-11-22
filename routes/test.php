be084863913a82ee68acfa7d90971d6c6d2a290f
wuyufan888 <152785469@qq.com>
2019-11-22 10:27:01 +0800

    add test.php

---
+++ routes/test.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('index', 'IndexController@index');
+Route::get('home', 'IndexController@home');
+Route::get('cctv','IndexController@cctv');
+Route::get('apk','IndexController@apk');
